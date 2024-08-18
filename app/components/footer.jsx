38b76c1694eb78585e6b27b9580ed51d770160c2
Jaydeep <jwagh@whileone.in>
2024-08-18 19:01:46 +0530

    Add version number to the footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,8 +3,12 @@ import { personalData } from '@/utils/data/personal-data';
 import Link from 'next/link';
 import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
+import getConfig from 'next/config';
 
 function Footer() {
+  const { publicRuntimeConfig } = getConfig();
+  const version = publicRuntimeConfig?.version
+
   return (
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
       <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
@@ -13,7 +17,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/jaydeepw/" className="text-[#16f2b3]">{personalData.name}</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/jaydeepw/" className="text-[#16f2b3]">{personalData.name}</Link> - v{version}
           </p>
           <div className="flex items-center gap-5">
             <Link
--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
-const path = require('path')
- 
+const path = require("path");
+const { version } = require("./package.json");
+
 module.exports = {
+  publicRuntimeConfig: {
+    version,
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
       },
     ],
   },
-}
+};
